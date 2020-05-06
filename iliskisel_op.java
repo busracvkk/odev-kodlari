da1b740481a3aefd1eb9d92747be33120f9c0466
busracvkk <64383791+busracvkk@users.noreply.github.com>
2020-05-07 00:58:42 +0300

    Add files via upload

---
+++ iliskisel_op.java
@@ -0,0 +1,20 @@
+package iliskisel_op;
+
+public class iliskisel_op {
+	public static void main(String[] args) {
+		
+		String A = "Ahmet", M = "Mehmet";
+		int    B = 38, N = 56, C = 56;
+		
+		
+		
+		System.out.println("" + (B != N));
+		System.out.println("M A eþit mi?  " + (M==A));
+		System.out.println("B N den büyük mü?  " + (B > N));
+		System.out.println("B N den küçük mü?  " + (B < N));
+		System.out.println("B N den büyük veya eþit mi?  " + (B>=N));
+		System.out.println("N C den küçük veya eþit mi?  " + (N<=C));
+		
+	}
+
+}
