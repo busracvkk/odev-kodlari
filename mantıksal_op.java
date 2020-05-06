0283728605a890d34ce7c901e0c7fe6f450e0e95
busracvkk <64383791+busracvkk@users.noreply.github.com>
2020-05-07 01:03:30 +0300

    Add files via upload

---
+++ mantıksal_op.java
@@ -0,0 +1,17 @@
+package mantýksal_op;
+
+public class mantýksal_op {
+	public static void main(String[] args) {
+		
+		
+		int    A = 18, B = 13, C = 35;
+		
+		
+		
+		System.out.println("" + ( A<20 && B==C ));
+		System.out.println("" + ( A<20 || B==C ));
+		System.out.println("" + ( A != B ));
+	
+	}
+
+}
